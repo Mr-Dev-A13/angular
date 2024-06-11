43f94830887bbaf45305e1d5894686d5104862fc
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-06-11 20:26:55 +0400

    feat(post): add interpolation for ng-post component

--- learning/angular_9/basics_and_syntax/src/app/post/post.component.ts
+++ learning/angular_9/basics_and_syntax/src/app/post/post.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
-import {NgPostComponent} from "../ng-post/ng-post.component";
+import { NgPostComponent } from "../ng-post/ng-post.component";
+import { TitleComponent } from "./title/title.component";
 
 @Component({
   selector: "app-post",
   standalone: true,
-  imports: [NgPostComponent],
+  imports: [NgPostComponent, TitleComponent],
   templateUrl: "./post.component.html",
   styleUrl: "./post.component.scss",
   // styleUrls: ["./post.component.scss"],
