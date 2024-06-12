8f7d9daae500759a41db1b30193a6856bd1a6f81
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-06-12 11:24:17 +0400

    feat(app): add event bindings and way binding

--- learning/angular_9/basics_and_syntax/src/app/app.component.ts
+++ learning/angular_9/basics_and_syntax/src/app/app.component.ts
@@ -5,10 +5,33 @@ import {PostComponent} from "./post/post.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostComponent],
+  imports: [RouterOutlet, PostComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'basics_and_syntax';
+  title: string = 'React';
+  inputValue: string = "";
+  imgPath: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcR5U16C8yXgBpl7-Bc7Itjx3_LRl425zINA&s";
+
+  constructor () {
+    setTimeout(() => {
+      this.title = "Angular";
+      this.imgPath = "https://yt3.googleusercontent.com/y4J_Fs5ksRlxx6_LzT1VKxVqH_T8Vyn_RN_YYgLJhuMzBS5qxTgm7NlEcMkQd3hgCpfWtYcEUg=s900-c-k-c0x00ffffff-no-rj";
+    }, 2000);
+  }
+
+  onInputHandler(event: Event) {
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+
+  onKeyUpHandler(input: any) {
+    // console.log(input.value);
+  }
+
+  onClickHandler () {
+    this.inputValue = "";
+  }
+
+
 }
