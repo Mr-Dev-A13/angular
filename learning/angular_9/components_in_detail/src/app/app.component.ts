bbea7debae431d93c931ca1f787c988b262e6f03
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-06-15 15:12:47 +0400

    feat(app): add passing parameters from app to post

--- learning/angular_9/components_in_detail/src/app/app.component.ts
+++ learning/angular_9/components_in_detail/src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+
+import { NgFor } from "@angular/common";
+
 import { RouterOutlet } from '@angular/router';
+import { PostComponent } from "./post/post.component";
+import { PostFormComponent } from "./post-form/post-form.component";
+
+export interface Posts {
+  id?: number | string,
+  title: string,
+  text: string
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    PostComponent,
+    PostFormComponent,
+    NgFor,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'components_in_detail';
+  posts: Array<Posts> = [
+    {id: 1, title: "Component", text: "Angular components are work!"},
+    {id: 2, title: "Parameters", text: "Angular parameters are work!"},
+  ]
 }
