25450c7a778b80272688bd0c3b140ec62d06e305
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-07-07 23:28:44 +0400

    feat(directives): add host binding into component

--- learning/angular_9/directives/src/app/directives/style.directives.ts
+++ learning/angular_9/directives/src/app/directives/style.directives.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from "@angular/core";
 
 @Directive({
   selector: "[appStyle]",
@@ -7,28 +7,36 @@ import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/co
 
 export class appStyleDirective {
   @Input("appStyle") color: string = "blue";
-  @Input() fontWeight: string = "normal";
+  @Input() dStyles: {border?: string, fontWeight?: string, borderRadius?: string} = {
+    border: "2px",
+    fontWeight: "normal",
+    borderRadius: "10px"
+  };
+  @HostBinding("style.color") elColor: string | null = null;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {
     this.renderer.setStyle(this.element.nativeElement, "fontSize", "20px");
     this.renderer.setStyle(this.element.nativeElement, "fontWeight", "normal");
-
-    console.log(this.color)
   }
 
   @HostListener("click", ["$event"]) onClick(event: Event) {
     // event.target;
-    console.log("The click worked");
     console.log(event);
   }
 
   @HostListener("mouseenter") onEnter () {
-    this.renderer.setStyle(this.element.nativeElement, "color", this.color);
-    this.renderer.setStyle(this.element.nativeElement, "fontWeight", this.fontWeight);
+    // this.renderer.setStyle(this.element.nativeElement, "color", this.color);
+    this.elColor = this.color;
+    this.renderer.setStyle(this.element.nativeElement, "border", this.dStyles.border);
+    this.renderer.setStyle(this.element.nativeElement, "fontWeight", this.dStyles.fontWeight);
+    this.renderer.setStyle(this.element.nativeElement, "borderRadius", this.dStyles.borderRadius);
   }
 
   @HostListener("mouseleave") onLeave () {
-    this.renderer.setStyle(this.element.nativeElement, "color", null);
+    // this.renderer.setStyle(this.element.nativeElement, "color", null);
+    this.elColor = null;
+    this.renderer.setStyle(this.element.nativeElement, "border", null);
     this.renderer.setStyle(this.element.nativeElement, "fontWeight", null);
+    this.renderer.setStyle(this.element.nativeElement, "borderRadius", null);
   }
 }
