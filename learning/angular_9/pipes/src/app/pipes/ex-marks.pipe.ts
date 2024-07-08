cb780eda7b43af6f67e8ab82b53f19c7edb24279
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-07-08 19:01:43 +0400

    feat(pipes): add ex-marks, filter, multiple pipes

---
+++ learning/angular_9/pipes/src/app/pipes/ex-marks.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'exMarks',
+  standalone: true
+})
+export class ExMarksPipe implements PipeTransform {
+  transform(str: string): string {
+    return `${str.trim()} !!!`;
+  }
+}
