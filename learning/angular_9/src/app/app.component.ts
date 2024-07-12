fd2f384b2f7e5874df1bba2ed84379365afb0bdb
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-07-12 23:53:43 +0400

    feat(services): add base of services inside app

--- learning/angular_9/src/app/app.component.ts
+++ learning/angular_9/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ServicesComponent } from "../base-services/services.component";
+import { AppCounterService } from "../base-services/services/app-counter.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ServicesComponent],
+  providers: [AppCounterService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
