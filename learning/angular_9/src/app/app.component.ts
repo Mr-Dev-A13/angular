00c5f8045c19c8ebb19a5561796f1aac23d48846
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-07-17 11:36:30 +0400

    feat(services): add log service into other service

--- learning/angular_9/src/app/app.component.ts
+++ learning/angular_9/src/app/app.component.ts
@@ -2,15 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ServicesComponent } from "../base-services/services.component";
 import { AppCounterService } from "../base-services/services/app-counter.service";
+import { LocalCounterService } from "../base-services/services/local-counter.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ServicesComponent],
-  providers: [AppCounterService],
+  providers: [LocalCounterService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular_9';
+
+  constructor(
+    protected appCounterServices: AppCounterService,
+    protected localCounterServices: LocalCounterService
+  ) {
+
+  }
 }
--- learning/angular_9/src/base-services/services/app-counter.service.ts
+++ learning/angular_9/src/base-services/services/app-counter.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
+import { LogService } from "./log.service";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class AppCounterService {
   counter = 0;
 
+  constructor(private logService: LogService) {
+  }
+
   increase () {
+    this.logService.log("increase of App");
     this.counter++;
   }
 
   decrease () {
+    this.logService.log("decrease of App");
     this.counter--;
   }
 }
