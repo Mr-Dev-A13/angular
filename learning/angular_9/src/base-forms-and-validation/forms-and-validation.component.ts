f852bc8a37d122951752b49eee628ebb6d2486cd
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-07-17 16:29:29 +0400

    add skeleton project for forms and validation folder

---
+++ learning/angular_9/src/base-forms-and-validation/forms-and-validation.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from "@angular/forms";
+
+@Component({
+  selector: 'app-forms-and-validation',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './forms-and-validation.component.html',
+  styleUrl: './forms-and-validation.component.scss'
+})
+
+export class FormsAndValidationComponent implements OnInit {
+  form!: FormGroup;
+
+  ngOnInit (): void {
+    this.form = new FormGroup({
+      email: new FormControl("", [
+        Validators.email,
+        Validators.required
+      ]),
+      password: new FormControl(null, [
+        Validators.minLength(6),
+        Validators.required
+      ])
+    });
+  }
+
+  submit (): void {
+    console.log("Form submitted ", this.form);
+    const formData = {...this.form.value};
+    console.log(formData);
+  }
+
+}
