59bb8238a5831203227f716c84e5bb230ce520cf
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-07-20 02:42:31 +0400

    build(http-client): add skeleton project for angular http client

--- learning/angular_9/src/app/app.component.ts
+++ learning/angular_9/src/app/app.component.ts
@@ -4,11 +4,17 @@ import { ServicesComponent } from "../base-services/services.component";
 import { AppCounterService } from "../base-services/services/app-counter.service";
 import { LocalCounterService } from "../base-services/services/local-counter.service";
 import { FormsAndValidationComponent } from "../base-forms-and-validation/forms-and-validation.component";
+import { BaseHttpClientComponent } from "../base-http-client/base-http-client.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ServicesComponent, FormsAndValidationComponent],
+  imports: [
+    RouterOutlet,
+    ServicesComponent,
+    FormsAndValidationComponent,
+    BaseHttpClientComponent
+  ],
   providers: [LocalCounterService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ learning/angular_9/src/base-http-client/base-http-client.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: 'app-base-http-client',
+  standalone: true,
+  imports: [ ],
+  templateUrl: './base-http-client.component.html',
+  styleUrl: './base-http-client.component.scss'
+})
+export class BaseHttpClientComponent {
+
+}
