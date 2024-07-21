e8e724063d1e53d4004f2f61c8dacacbbb77c9f0
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-07-21 20:47:11 +0400

    build(http-client): add get request with subscribe

--- learning/angular_9/src/base-http-client/base-http-client.component.ts
+++ learning/angular_9/src/base-http-client/base-http-client.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { NgFor, TitleCasePipe } from "@angular/common";
+
+export interface IToDo {
+  id?: number,
+  title: string,
+  completed: boolean,
+}
 
 @Component({
   selector: 'app-base-http-client',
   standalone: true,
-  imports: [ ],
+  imports: [NgFor, TitleCasePipe],
   templateUrl: './base-http-client.component.html',
   styleUrl: './base-http-client.component.scss'
 })
-export class BaseHttpClientComponent {
 
+export class BaseHttpClientComponent implements OnInit {
+  todoData: IToDo[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http?.get<IToDo[]>("https://jsonplaceholder.typicode.com/todos?_limit=2")
+      .subscribe(todo => {
+        this.todoData = todo;
+      })
+  }
 }
