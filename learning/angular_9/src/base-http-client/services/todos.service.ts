ddddff11998c3eca10f752cc04935469de3ee9ae
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-07-25 17:05:59 +0400

    feat(services): add params and observe into http events

--- learning/angular_9/src/base-http-client/services/todos.service.ts
+++ learning/angular_9/src/base-http-client/services/todos.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { catchError, delay, Observable, throwError } from "rxjs";
+import {HttpClient, HttpEvent, HttpEventType, HttpHeaders, HttpParams, HttpResponse} from "@angular/common/http";
+import {catchError, delay, map, Observable, tap, throwError} from "rxjs";
 
 export interface IToDo {
   id?: number,
@@ -13,23 +13,53 @@ export class TodosService {
   constructor (private http: HttpClient) {}
 
   fetchTodos (): Observable<IToDo[]> {
-    return this.http?.get<IToDo[]>("https://jsonplaceholder.typicode.com/todos?_limit=3")
+    let params: HttpParams;
+    // Using (---> set <---) method of HttpParams
+    params = new HttpParams().set("_limit", "3");
+    // Using (---> append <---) method of HttpParams
+    // params = new HttpParams().append("_limit", "3");
+
+    return this.http?.get<IToDo[]>("https://jsonplaceholder.typicode.com/todos", {
+      params,
+      observe: "response"
+    })
       .pipe(
-        delay(1500),
+        map((response: HttpResponse<IToDo[]>) => {
+          // console.log("Response(map) ===>", response);
+          return <IToDo[]> response.body
+        }),
+        delay<IToDo[]>(1500),
         catchError(error => {
-          console.log("Error ===>", error.message);
+          // console.log("Error ===>", error.message);
           return throwError(error);
         })
       );
   }
 
   addTodo (todo: IToDo): Observable<IToDo> {
-    return this.http?.post<IToDo>("https://jsonplaceholder.typicode.com/todos", todo)
+    return this.http?.post<IToDo>("https://jsonplaceholder.typicode.com/todos", todo, {
+      headers: new HttpHeaders({
+        CustomHeaderProperty: "A13"
+      })
+    })
 
   }
 
-  removeTodo(id?: number): Observable<IToDo[]> {
-    return this.http?.delete<IToDo[]>(`https://jsonplaceholder.typicode.com/todos/${id}`)
+  removeTodo(id?: number): Observable<HttpEvent<IToDo[]>> {
+    return this.http?.delete<IToDo[]>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      observe: "events"
+    })
+      .pipe(
+        tap((event: HttpEvent<IToDo[]>) => {
+          if (event.type === HttpEventType.Sent) {
+            // console.log("Sent Event", event);
+          }
+
+          if (event.type === HttpEventType.Response) {
+            // console.log("Response Event", event);
+          }
+        })
+      )
   }
 
   completedTodo(id?: number): Observable<IToDo> {
