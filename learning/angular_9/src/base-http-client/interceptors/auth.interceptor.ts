d6987b3c5cac7b2385f9984409e9aedd1386e69e
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-08-01 23:56:51 +0400

    feat(interceptor): add auth interceptor

--- learning/angular_9/src/base-http-client/interceptors/auth.interceptor.ts
+++ learning/angular_9/src/base-http-client/interceptors/auth.interceptor.ts
@@ -4,7 +4,6 @@ import {Observable} from "rxjs";
 
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
     return next.handle(req);
   }
 }
