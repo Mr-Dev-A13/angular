df77ad2ec84084b243aa82a6cc912db5cf0442ee
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-08-04 19:29:39 +0400

    feat(interceptor): add with intercepter from di into app config

--- learning/angular_9/src/app/app.config.ts
+++ learning/angular_9/src/app/app.config.ts
@@ -1,13 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, Provider, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from "@angular/common/http";
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 
 import { routes } from './app.routes';
+import {AuthInterceptor} from "../base-http-client/interceptors/auth.interceptor";
+
+const INTERCEPTORS_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: AuthInterceptor,
+  multi: true
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptorsFromDi()),
+    INTERCEPTORS_PROVIDER
   ]
 };
