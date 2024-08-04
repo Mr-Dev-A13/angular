b577ac8116951c1f703887cf1154140784a384e1
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-08-04 19:29:39 +0400

    feat(interceptor): add with intercepter from di into app config

--- learning/angular_9/src/base-http-client/interceptors/auth.interceptor.ts
+++ learning/angular_9/src/base-http-client/interceptors/auth.interceptor.ts
@@ -1,11 +1,18 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable, tap} from "rxjs";
 
 
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const clone = req.clone({
+      headers: req.headers.append("new-name", "Append new name"),
+    })
 
-    
-    return next.handle(req);
+    return next.handle(clone).pipe(tap(event => {
+        if (event.type === HttpEventType.Response) {
+          console.log("next.handle(clone).pipe(tap())", event);
+        }
+      }
+    ));
   }
 }
