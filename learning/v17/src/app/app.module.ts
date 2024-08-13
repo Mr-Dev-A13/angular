69ab973894afcf2c311b8f5605694cc9c2271a1c
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-08-14 00:50:28 +0400

    feat(app): add routing links with active link and active options

--- learning/v17/src/app/app.module.ts
+++ learning/v17/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from "./home/home.component";
+import { PostsComponent } from "./posts/posts.component";
+import { NgFor } from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PostsComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
