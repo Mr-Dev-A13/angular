1f192f7dd834da2f6f82e1fc03846e2e2adcebb3
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2024-09-30 18:19:35 +0400

    feat(routing): add routing for project with more features
    The following functionality and changes have been added
    
    active link, dynamic switch pages and routs, program navigation, processing parameters, working with parameters, nested pages, redirect and error handling, page protection, protection of child routes, resolvers work.

--- learning/v17/src/app/app.module.ts
+++ learning/v17/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from "./home/home.component";
 import { PostsComponent } from "./posts/posts.component";
-import { FormsModule } from "@angular/forms";
+import { PostComponent } from "./post/post.component";
+import { AboutComponent } from "./about/about.component";
+import { AboutExtraComponent } from "./about-extra/about-extra.component";
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PostsComponent,
+    PostComponent,
+    AboutComponent,
+    AboutExtraComponent,
+    ErrorPageComponent,
 
   ],
   imports: [
