30cc73395b0b4eac043512628393241c112d3642
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2025-03-02 16:46:17 +0400

    feat(tasks): add dummy tasks data and corresponding view

--- learning/v17/angular-essentials_components-templates-services_more/src/app/tasks/tasks.component.ts
+++ learning/v17/angular-essentials_components-templates-services_more/src/app/tasks/tasks.component.ts
@@ -1,12 +1,22 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+import { DUMMY_TASKS, IDummyTasks } from "../dummy-tasks";
 
 @Component({
     selector: 'app-tasks',
     standalone: true,
-    imports: [],
+    imports: [
+        TaskComponent
+    ],
     templateUrl: './tasks.component.html',
     styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
+    @Input({required: true}) userId!: string;
     @Input({ required: true }) name?: string;
+    tasks: IDummyTasks[] = DUMMY_TASKS;
+
+    get selectedUsersTasks(): IDummyTasks[] {
+        return this.tasks.filter((task: IDummyTasks): boolean => task.userId === this.userId)
+    }
 }
