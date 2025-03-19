4049389f7366c5177a7feedd5630c969b8ad8c1d
Mr-Dev-A13 <avik.ghukasyan13@gmail.com>
2025-03-19 23:00:10 +0400

    feat(tasks): add service of tasks and change methods from component to service

--- learning/v17/angular-essentials_components-templates-services_more/src/app/tasks/tasks.component.ts
+++ learning/v17/angular-essentials_components-templates-services_more/src/app/tasks/tasks.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
-import { DUMMY_TASKS } from "../dummy-tasks";
 import { type ITask } from "./task/task.model";
 import { NewTaskComponent } from "./new-task/new-task.component";
 import { type NewTaskData } from "./new-task/new-task.model";
